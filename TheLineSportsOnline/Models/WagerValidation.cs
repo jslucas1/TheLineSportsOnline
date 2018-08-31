115cf17126190ae142eeef3aca5810fcf4452795
Jeff Lucas <jslucas1@msn.com>
2018-08-31 18:52:45 -0500

    change cutoff

--- TheLineSportsOnline/Models/WagerValidation.cs
+++ TheLineSportsOnline/Models/WagerValidation.cs
@@ -58,7 +58,7 @@ namespace TheLineSportsOnline.Models
             TimeSpan cutOfTime = new TimeSpan(17, 0, 0); //5 o'clock
             TimeSpan now = DateTime.Now.TimeOfDay;
 
-            if ((day == "friday" && (now > cutOfTime)) || day == "saturday" || day == "sunday")
+            if (day == "sunday")
             {
                 return new ValidationResult(ERR_BET_LOCK);
             }
