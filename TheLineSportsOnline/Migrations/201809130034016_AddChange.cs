6105d159722fa14cb9878c1106999329fbf0aec0
Austin Lucas <aclucas196@gmail.com>
2018-09-12 19:39:02 -0500

    migrations

---
+++ TheLineSportsOnline/Migrations/201809130034016_AddChange.cs
@@ -0,0 +1,16 @@
+namespace TheLineSportsOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddChange : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
